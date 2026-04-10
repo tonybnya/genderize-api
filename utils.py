28606dd1772e62d82f670ec21d941a994d3f6f27
tonybnya <tonybnya@gmail.com>
2026-04-10 15:44:18 +0100

    feat: add function to get current timestamp

--- utils.py
+++ utils.py
@@ -3,3 +3,8 @@ Script Name : utils.py
 Description : Helper functions
 Author      : @tonybnya
 """
+from datetime import datetime, timezone
+
+def current_timestamp() -> str:
+    """Generate ISO 8601 UTC timestamp."""
+    return datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
